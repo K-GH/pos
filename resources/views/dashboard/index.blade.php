86b48b1aff0b9c9d76692ee901dc82cf34109ba9
K-GH <khalled.ghonaim95@gmail.com>
2020-06-13 06:34:08 +0200

    'setupDashboardLayouts'

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.dashboard.app')
+@section('content')
+
+<div class="content-wrapper">
+    <section class="content-header">
+        <h1>blank page
+            <small>it all start here</small>
+        </h1>
+
+        <ol class="breadcrumb">
+            <li class="active"> <i class="fa fa-dashboard"> </i> @lang('site.dashboard')</li>
+        </ol>
+    </section>
+
+    <section class="content">
+        <h1>this is index of dashboard</h1>
+    </section>
+        
+</div>
+
+
+@endsection
--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -2,8 +2,11 @@
 
 Route::prefix('dashboard')->name('dashboard.')->group(function(){
 
-    Route::get('/check', function () {
-        return 'this is dashboard';
-    });
+   /* Route::get('/check', function () {
+        //return 'this is dashboard';
+        return view('dashboard.index');
+    });*/
+
+    Route::get('index', 'DashboardController@index')->name('dashboard.index');
 
 });
